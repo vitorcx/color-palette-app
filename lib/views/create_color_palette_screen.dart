a046b2c7d9f6f0d6ddd5089e1cce2fb3ec9eb6cb
Vítor Cardoso Xoteslem <vitorcx010@gmail.com>
2021-07-01 11:02:21 -0300

    Ajustando tela de criaçao de paletas

--- lib/views/create_color_palette_screen.dart
+++ lib/views/create_color_palette_screen.dart
@@ -18,52 +18,59 @@ class _CreateColorPaletteScreenState extends State<CreateColorPaletteScreen> {
 
   @override
   Widget build(BuildContext context) {
+    AppBar appBar =
+        AppBar(title: Text('Nova Paleta de Cores'), centerTitle: true);
+    double screenHeight = MediaQuery.of(context).size.height -
+        appBar.preferredSize.height -
+        MediaQuery.of(context).padding.top;
     print(_controller.text);
     return Scaffold(
-      appBar: AppBar(title: Text('Nova Paleta de Cores'), centerTitle: true),
-      body: Column(mainAxisSize: MainAxisSize.max, children: [
-        Center(
-          child: Column(
-            children: [
-              TextField(
-                textAlign: TextAlign.center,
-                controller: _controller,
+      appBar: appBar,
+      body: SingleChildScrollView(
+        child: Container(
+          height: screenHeight,
+          alignment: Alignment.bottomCenter,
+          child: Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+            TextField(
+              textAlign: TextAlign.center,
+              controller: _controller,
+            ),
+            Column(
+              children: [
+                for (int i = 0; i < 5; i++) ColorField(index: i),
+              ],
+            ),
+            IconButton(
+              alignment: Alignment.center,
+              icon: Icon(Icons.refresh),
+              onPressed: () {},
+              color: Colors.black,
+              iconSize: 80,
+            ),
+            SizedBox(
+              height: 100,
+              width: MediaQuery.of(context).size.width,
+              child: ElevatedButton(
+                onPressed: () {
+                  List<int> colors =
+                      BlocProvider.of<ColorsFormBloc>(context).state.colors;
+                  final newColorPalette = ColorPalette(
+                      id: '', colors: colors, title: _controller.text);
+                  BlocProvider.of<ColorPaletteBloc>(context)
+                      .add(ColorPaletteCreate(newColorPalette));
+                  Navigator.of(context).pop();
+                },
+                child: Text("SALVAR"),
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.black,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(0.0),
+                        side: BorderSide(color: Colors.black))),
               ),
-              Column(
-                children: [
-                  for (int i = 0; i < 5; i++) ColorField(index: i),
-                ],
-              ),
-            ],
-          ),
+            ),
+          ]),
         ),
-        IconButton(
-          icon: Icon(Icons.refresh),
-          onPressed: () {},
-          color: Colors.black,
-        ),
-        Container(
-          height: 100,
-          width: MediaQuery.of(context).size.width,
-          child: ElevatedButton(
-            onPressed: () {
-              List<int> colors =
-                  BlocProvider.of<ColorsFormBloc>(context).state.colors;
-              final newColorPalette =
-                  ColorPalette(id: '', colors: colors, title: _controller.text);
-              BlocProvider.of<ColorPaletteBloc>(context)
-                  .add(ColorPaletteCreate(newColorPalette));
-              Navigator.of(context).pop();
-            },
-            child: Text("SALVAR"),
-            style: ElevatedButton.styleFrom(
-                primary: Colors.black,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(0.0),
-                    side: BorderSide(color: Colors.black))),
-          ),
-        )
-      ]),
+      ),
     );
   }
 
