c1567d36f75430bc7f8900857e9264f9a104ae09
Vítor Cardoso Xoteslem <vitorcx010@gmail.com>
2021-07-08 17:28:40 -0300

    Ajustando cor do texto de acordo com o fundo

--- lib/views/color_field.dart
+++ lib/views/color_field.dart
@@ -12,13 +12,21 @@ class ColorField extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<ColorsFormBloc, ColorsFormState>(
       builder: (context, state) {
+        Color color = Color(state.colors[index]).withAlpha(0xff);
+        Color textColor =
+            color.computeLuminance() < 0.2 ? Colors.white : Colors.black;
         return Container(
             padding: EdgeInsets.all(10),
-            color: Color(state.colors[index]).withAlpha(0xff),
+            color: color,
             child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text('${state.colors[index]}'),
+                Text(
+                  'R: ${color.red}   G: ${color.green}   B: ${color.blue}',
+                  style: TextStyle(color: textColor, fontSize: 18),
+                ),
                 IconButton(
+                    color: textColor,
                     onPressed: () {
                       BlocProvider.of<ColorsFormBloc>(context).add(
                           ChangeColorsFormEvent(
